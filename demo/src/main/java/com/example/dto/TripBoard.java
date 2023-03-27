4002f96dfe71112e84464b795d7b98d4a09074e2
leeeeeunj <qwas2555@naver.com>
2023-03-27 20:55:07 +0900

    3/27

--- demo/src/main/java/com/example/dto/TripBoard.java
+++ demo/src/main/java/com/example/dto/TripBoard.java
@@ -1,5 +1,21 @@
 package com.example.dto;
 
-public class TripBoard {
 
+public class TripBoard {
+	private int t_bo_no;
+	private String t_bo_title;
+	private String t_bo_content;
+	private Date t_bo_date;
+	private int t_bo_readcount;
+	private int t_bo_status;
+	private int t_bo_person;
+	private String t_bo_img;
+	private Date t_bo_start_date;
+	private Date t_bo_end_date;
+	private String t_bo_style;
+	private String hashtag;
+	//FK
+	private int city_no;
+	private String user_id;
+	
 }
